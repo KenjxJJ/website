8892e97491515fce8cfb23cf90a422f6a9cc666c
JOEL KIGGWE <joelkiggwe94@gmail.com>
2019-10-15 22:50:22 +0300

    Add bootstrap package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { MyNetflixAppComponent } from './mynetflix-app.component';
+import { MoviesListComponent } from './movies/movies-list.component';
+
+
 
 @NgModule({
   declarations: [
-  MyNetflixAppComponent
+  MyNetflixAppComponent,
+  MoviesListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/movies/movies-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'movies-list',
+     templateUrl: './movies-list.component.html',
+     styleUrls: ['./movies-list.component.css']
+})
+
+export class MoviesListComponent {
+
+}
