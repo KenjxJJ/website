b789622e280b680b35c5bdc4b707a06c352f77ac
JOEL KIGGWE <joelkiggwe94@gmail.com>
2019-10-15 22:10:03 +0300

    Add package @ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { MyNetflixAppComponent } from './mynetflix-app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { MyNetflixAppComponent } from './mynetflix-app.component';
   MyNetflixAppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [MyNetflixAppComponent]
