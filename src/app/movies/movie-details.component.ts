6974d343410b3310cce321473c4ff725d39fde29
JOEL KIGGWE <joelkiggwe94@gmail.com>
2019-10-23 03:52:17 +0300

    Style the movie-details component

--- src/app/movies/movie-details.component.ts
+++ src/app/movies/movie-details.component.ts
@@ -1,19 +1,50 @@
-import { Component, OnInit, EventEmitter, Output} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { MoviesService } from './shared/movies.service';
 import { IMovies } from './shared/movies.model';
 import {  ActivatedRoute } from '@angular/router';
-
 import { FavMovieService } from './fav_movies/fav-movies.service';
 
 @Component({
       templateUrl: './movie-details.component.html',
       styles : [`
-        img [src] {
-            float: right;
-        }
-        .container {
-            background-color : black;
+      * {
+        box-sizing: unset;
+       }
+         .container {
+            background-color : rgb(67,67,67);
             color : white;
+            min-width: 600px;
+            width: 100%;
+        }
+
+        ul li{
+           padding-left: -7px;
+
+        }
+        button.btn {
+           background-color: rgba(177, 67, 20, 0.7);
+           color: white;
+           margin: 2px;
+           font-size: 12px;
+           height: 20px;
+        }
+         div {
+            float: left;
+            padding-right: 10px;
+         }
+        @media screen and ( max-width: 600px ) {
+            img {
+             width:100%;
+             float: left:
+             margin-right: 10px;
+           }
+        }
+        @media screen and (min-width: 601px){
+          img {
+            width:90%;
+            float: left:
+            margin-right: 10px;
+          }
         }
          `]
 })
@@ -21,18 +52,16 @@ import { FavMovieService } from './fav_movies/fav-movies.service';
 export class MovieDetailsComponent implements OnInit {
 
   movie: IMovies;
-  genres: any[] = [];
   myFavMoviesList: IMovies[];
-  //isClicked: Boolean = false;
   id: string;
-  myFavMovie: IMovies;
 
-  constructor( private moviesService: MoviesService, private route: ActivatedRoute, private favMoviesService: FavMovieService) {}
+  constructor( private moviesService: MoviesService, private route: ActivatedRoute,
+    private favMoviesService: FavMovieService) {}
 
     ngOnInit() {
       this.movie = this.moviesService.getMovie(this.route.snapshot.params.objectId);
       this.id = this.route.snapshot.params['objectId'];
-      this.genres = this.moviesService.getMovieGenre(this.movie.objectId);
+
 
     }
     addToMyFav() {
