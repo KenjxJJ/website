9e63ac249360e3c1c75b8341e0a898f00393c353
JOEL KIGGWE <joelkiggwe94@gmail.com>
2020-09-22 15:32:44 +0300

    Comment default angular testing file

--- src/app/mynetflix-app.component.spec.ts
+++ src/app/mynetflix-app.component.spec.ts
@@ -1,33 +1,33 @@
-import { TestBed, async } from '@angular/core/testing';
-import { MyNetflixAppComponent } from './mynetflix-app.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+// import { TestBed, async } from '@angular/core/testing';
+// import { MyNetflixAppComponent } from './mynetflix-app.component';
+// import { NO_ERRORS_SCHEMA } from '@angular/core';
 
-describe('MyNetFlixAppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-       MyNetflixAppComponent
-      ],
-      schemas: [NO_ERRORS_SCHEMA]
-    }).compileComponents();
-  }));
+// describe('MyNetFlixAppComponent', () => {
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [
+//        MyNetflixAppComponent
+//       ],
+//       schemas: [NO_ERRORS_SCHEMA]
+//     }).compileComponents();
+//   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(MyNetflixAppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+//   it('should create the app', () => {
+//     const fixture = TestBed.createComponent(MyNetflixAppComponent);
+//     const app = fixture.debugElement.componentInstance;
+//     expect(app).toBeTruthy();
+//   });
 
-  it('should have as title "My Netflix"', () => {
-    const fixture = TestBed.createComponent(MyNetflixAppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('My Netflix');
-  });
+//   it('should have as title "My Netflix"', () => {
+//     const fixture = TestBed.createComponent(MyNetflixAppComponent);
+//     const app = fixture.debugElement.componentInstance;
+//     expect(app.title).toEqual('My Netflix');
+//   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(MyNetflixAppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('My Netflix');
-  });
-});
+//   it('should render title', () => {
+//     const fixture = TestBed.createComponent(MyNetflixAppComponent);
+//     fixture.detectChanges();
+//     const compiled = fixture.debugElement.nativeElement;
+//     expect(compiled.querySelector('h1').textContent).toContain('My Netflix');
+//   });
+// });
